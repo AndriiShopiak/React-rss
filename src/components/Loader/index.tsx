6b8a773d644e7f12154585d5fbbd2a6cf9f2f641
Andrii Shopiak <andriyshopyak@gmail.com>
2024-09-24 09:46:22 +0300

    refactor: change Loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,9 +1,7 @@
-import React, { ReactNode } from 'react';
 import './loader.css';
 
-class Loader extends React.Component {
-  render(): ReactNode {
-    return <h1 className="loaderTitle">Loading...</h1>;
-  }
+function Loader() {
+  return <h1 className="loaderTitle">Loading...</h1>;
 }
+
 export default Loader;

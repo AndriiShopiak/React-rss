0e9e2a5c22aa3badf61c484125a7c7aaf049aa4b
Andrii Shopiak <andriyshopyak@gmail.com>
2024-10-05 16:08:22 +0300

    feat: add functional component for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Component, ReactNode } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import SearchInput from './components/SearchInput';
 import ResultTab from './components/ResultTab';
@@ -8,66 +8,61 @@ import './assets/styles/global.css';
 import ErrorBoundary from './components/ErrorBoundary/errorBoundary';
 import ErrorBtnGenerate from './components/ErrorBtnGenerate/errorBtnGenerate';
 
-interface AppProps {}
 interface AppState {
   searchResults: dataList;
-  isLoading: boolean;
-  hasError: boolean;
 }
 
-class App extends Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    this.getPeopleSearch = this.getPeopleSearch.bind(this);
-    this.state = {
-      searchResults: {
-        count: 0,
-        next: '',
-        previous: '',
-        results: [],
-      },
-      isLoading: false,
-      hasError: false,
-    };
-  }
-  componentDidMount(): void {
+function App() {
+  const [result, setResult] = useState<AppState>({
+    searchResults: {
+      count: 0,
+      next: '',
+      previous: '',
+      results: [],
+    },
+  });
+  const [isLoading, setLoading] = useState({ isLoading: false });
+
+  useEffect(() => {
     if (localStorage.getItem('value')) {
-      this.getPeopleSearch(localStorage.getItem('value') || '');
+      getPeopleSearch(localStorage.getItem('value') || '');
     } else {
-      this.getPeople();
+      getPeople();
     }
-  }
-  async getPeople() {
-    this.setState({ isLoading: true });
+  }, []);
+
+  async function getPeople() {
+    setLoading({ isLoading: true });
     try {
       const result = await axios.get('https://swapi.dev/api/people');
-      this.setState({ searchResults: result.data, isLoading: false });
+      setResult({ searchResults: result.data });
+      setLoading({ isLoading: false });
       return result;
     } catch {
       console.log('Something went wrong for getPeople request');
     }
   }
-  async getPeopleSearch(searchWord: string) {
+  async function getPeopleSearch(searchWord: string) {
     const searchUrl: string = 'https://swapi.dev/api/people/?search=' + `${searchWord.trim()}`;
-    this.setState({ isLoading: true });
+    setLoading({ isLoading: true });
 
     try {
       const result = await axios.get(searchUrl);
-      this.setState({ searchResults: result.data, isLoading: false });
+      setResult({ searchResults: result.data });
+      setLoading({ isLoading: false });
       return result;
     } catch {
       console.log('Something went wrong for getPeopleSearch request');
     }
   }
-  render(): ReactNode {
-    return (
-      <ErrorBoundary>
-        <h1 className="mainTitle">Welcome to Star Wars Search</h1>
-        <SearchInput onSearch={this.getPeopleSearch} />
-        <ErrorBtnGenerate />
-        {this.state.isLoading ? <Loader /> : <ResultTab value={this.state.searchResults} />}
-      </ErrorBoundary>
-    );
-  }
+
+  return (
+    <ErrorBoundary>
+      <h1 className="mainTitle">Welcome to Star Wars Search</h1>
+      <SearchInput onSearch={getPeopleSearch} />
+      <ErrorBtnGenerate />
+      {isLoading.isLoading ? <Loader /> : <ResultTab value={result.searchResults} />}
+    </ErrorBoundary>
+  );
 }
 export default App;
